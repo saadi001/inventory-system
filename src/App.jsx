9176f143c0111d644880e6cf50e5a9edbe466f9d
saadi001 <sheikhsadi1620@gmail.com>
2025-06-06 04:06:41 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-center">Hello world!</h1>
+      <div className="flex items-center w-full mt-4 justify-center">
+        <Button>Hello</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
